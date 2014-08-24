26a8ee8fc258eb285fe405225c3a951632f9d2c0
Dan Lewis-Toakley <d.lewistoakley@gmail.com>
2014-08-24 19:59:56 +0200

    Parsing username attempt 1

--- js/app.js
+++ js/app.js
@@ -1,2 +1,16 @@
+// Add username
 
+
+
+$(document).ready(function() {
+	$.getJSON('data/userSearch.json', function(data) {
+		console.log('success');
+		$.each(data.items, function(i, item) {
+			$('.username').text(item.login);
+		});
+	}).error(function(jqXHR, textStatus, errorThrown) {
+        console.log("error: " + textStatus);
+        console.log("incoming data: " + jqXHR.responseText);
+    });
+});
 
